a611a07d0d5e70e14fe05f7994b03ea00cffd86d
naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
2021-01-04 19:39:58 +0530

    [buy2get1free] Remove getters to use for..in

--- buy2get1free/script.js
+++ buy2get1free/script.js
@@ -1,15 +1,6 @@
 let generateRandomNumber = number => Math.floor(Math.random()*number)
 const items = {
-    _appliances: ['television','fan','fridge','washing machine','camera','piano','radio','computer','air conditioner','clock'],
-    _furnitures: ['sofa','chair','desk','table','door','window','drawer','bed','rug','carpet'],
-    _stationery: ['pen','notebook','pencil','eraser','whiteboard','sharpie','brush','white out','file'],
-    get _appliances() {
-        return this._appliances
-    },
-    get _furnitures() {
-        return this._furnitures
-    },
-    get _stationery() {
-        return this._stationery
-    }
+    appliances: ['television','fan','fridge','washing machine','camera','piano','radio','computer','air conditioner','clock'],
+    furnitures: ['sofa','chair','desk','table','door','window','drawer','bed','rug','carpet'],
+    stationery: ['pen','notebook','pencil','eraser','whiteboard','sharpie','brush','white out','file']
 }
