bca865c95623a5dd67a62d80a69aa8ddfc7dd734
naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
2021-01-04 19:58:28 +0530

    [buy2get1free] Store and create message

--- buy2get1free/script.js
+++ buy2get1free/script.js
@@ -4,3 +4,20 @@ const items = {
     furnitures: ['sofa','chair','desk','table','door','window','drawer','bed','rug','carpet'],
     stationery: ['pen','notebook','pencil','eraser','whiteboard','sharpie','brush','white out','file']
 }
+let arrItems = []
+for(let key in items) {
+    let randomIndex = generateRandomNumber(items[key].length)
+    switch(key) {
+        case 'appliances':
+            arrItems.push(`Buy a ${items[key][randomIndex]}`)
+            break
+        case 'furnitures':
+            arrItems.push(`and a ${items[key][randomIndex]}`)
+            break
+        case 'stationery':
+            arrItems.push(`and get a ${items[key][randomIndex]} for free!`)
+            break
+        default:
+            arrItems.push('There is not enough info.')
+    }
+}
