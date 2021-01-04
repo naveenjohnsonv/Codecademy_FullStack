0138312af27ac44485e06d570d4df86704e47ad0
naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
2021-01-04 19:29:27 +0530

    [buy2get1free] Add object

--- buy2get1free/script.js
+++ buy2get1free/script.js
@@ -1,1 +1,15 @@
 let generateRandomNumber = number => Math.floor(Math.random()*number)
+const items = {
+    _appliances: ['television','fan','fridge','washing machine','camera','piano','radio','computer','air conditioner','clock'],
+    _furnitures: ['sofa','chair','desk','table','door','window','drawer','bed','rug','carpet'],
+    _stationery: ['pen','notebook','pencil','eraser','whiteboard','sharpie','brush','white out','file'],
+    get _appliances() {
+        return this._appliances
+    },
+    get _furnitures() {
+        return this._furnitures
+    },
+    get _stationery() {
+        return this._stationery
+    }
+}
