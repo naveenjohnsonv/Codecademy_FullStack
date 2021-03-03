d8452d0bf87b7dcf049b8779b1d4deb859202211
naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
2021-03-04 00:13:22 +0530

    [find-your-hat] implemented code for traversing path

--- find-your-hat/main.js
+++ find-your-hat/main.js
@@ -14,3 +14,54 @@ class Field {
   }
 
 }
+
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+
+let r = 0;
+let c = 0;
+let f = 0;
+
+do {
+  let input = prompt('Which way ([u]p/[d]own/[l]eft/[r]ight)? ');
+  switch(input) {
+    case 'u':
+    case 'U':
+      r--;
+      break;
+    case 'd':
+    case 'D':
+      r++;
+      break;
+    case 'r':
+    case 'R':
+      c++;
+      break;
+    case 'l':
+    case 'L':
+      c--;
+      break;
+  }
+  if(r < 0 || r >= myField.field.length || c < 0 || c >= myField.field[0].length) {
+    console.log('Out of bounds instruction! Try again.');
+    f++;
+  }
+  else
+  if(myField.field[r][c]===hole) {
+    console.log('Sorry, you fell into a hole! Try again.');
+    f++;
+  }
+  else
+  if(myField.field[r][c]===hat) {
+    console.log('Congrats, you found the hat!');
+    f++;
+  }
+  else {
+    myField.field[r][c] = pathCharacter;
+    myField.print();
+  }
+}
+while(!f);
